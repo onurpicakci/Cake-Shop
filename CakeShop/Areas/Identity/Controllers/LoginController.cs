63a91ece7060f8c15b75f514a03a858d5e0c2582
Onur Pıçakcı <98148844+onurpicakci@users.noreply.github.com>
2023-10-09 10:41:34 +0300

    edited login and register page

--- CakeShop/Areas/Identity/Controllers/LoginController.cs
+++ CakeShop/Areas/Identity/Controllers/LoginController.cs
@@ -1,49 +1,45 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CakeShop.Areas.Identity.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CakeShop.Areas.Identity.Controllers
+namespace CakeShop.Areas.Identity.Controllers;
+
+[Area("Identity")]
+[Route("Identity/[controller]/[action]")]
+[AllowAnonymous]
+public class LoginController : Controller
 {
-    [Area("Identity")]
-    [Route("Identity/[controller]/[action]")]
-    [AllowAnonymous]
-    public class LoginController : Controller
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public LoginController(SignInManager<IdentityUser> signInManager)
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        _signInManager = signInManager;
+    }
 
-        public LoginController(SignInManager<IdentityUser> signInManager)
-        {
-            _signInManager = signInManager;
-        }
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
+    [HttpPost]
+    public async Task<IActionResult> Index(UserLoginViewModel model)
+    {
+        if (ModelState.IsValid)
         {
-            return View();
-        }
-        
-        [HttpPost]
-        public async Task<IActionResult> Index(UserLoginViewModel model)
-        {
-            if (ModelState.IsValid)
+            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, true, true);
+            if (result.Succeeded)
             {
-                var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, true, true);
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("Index", "Home", new {area = "Admin"});
-                }
-                else
-                {
-                    ModelState.AddModelError("", "Username or Password is incorrect");
-                    return View("Index");
-                }
+                return RedirectToAction("Index", "Home", new { area = "Admin" });
             }
-            return View();
+            else
+            {
+                ModelState.AddModelError("", "Username or Password is incorrect");
+                return View("Index");
+            }
         }
+
+        return View();
     }
 }
--- CakeShop/Areas/Identity/Controllers/RegisterController.cs
+++ CakeShop/Areas/Identity/Controllers/RegisterController.cs
@@ -1,61 +1,57 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CakeShop.Areas.Identity.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CakeShop.Areas.Identity.Controllers
+namespace CakeShop.Areas.Identity.Controllers;
+
+[Area("Identity")]
+[Route("Identity/[controller]/[action]")]
+[AllowAnonymous]
+public class RegisterController : Controller
 {
-    [Area("Identity")]
-    [Route("Identity/[controller]/[action]")]
-    [AllowAnonymous]
-    public class RegisterController : Controller
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public RegisterController(UserManager<IdentityUser> userManager)
     {
-        private readonly UserManager<IdentityUser> _userManager;
+        _userManager = userManager;
+    }
 
-        public RegisterController(UserManager<IdentityUser> userManager)
-        {
-            _userManager = userManager;
-        }
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        return View(new UserRegisterViewModel());
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
+    [HttpPost]
+    public async Task<IActionResult> Index(UserRegisterViewModel model)
+    {
+        if (ModelState.IsValid)
         {
-            return View(new UserRegisterViewModel());
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Index(UserRegisterViewModel model)
-        {
-            if (ModelState.IsValid)
+            var user = new IdentityUser
             {
-                var user = new IdentityUser
-                {
-                    UserName = model.UserName,
-                    Email = model.Email,
-                    PhoneNumber = model.PhoneNumber,
-                };
+                UserName = model.UserName,
+                Email = model.Email,
+                PhoneNumber = model.PhoneNumber,
+            };
 
-                if (model.Password == model.ConfirmPassword)
+            if (model.Password == model.ConfirmPassword)
+            {
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
                 {
-                    var result = await _userManager.CreateAsync(user, model.Password);
-                    if (result.Succeeded)
+                    return RedirectToAction("Index", "Login", new { area = "Identity" });
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
                     {
-                        return RedirectToAction("Index", "Login", new { area = "Identity" });
-                    }
-                    else
-                    {
-                        foreach (var item in result.Errors)
-                        {
-                            ModelState.AddModelError("", item.Description);
-                        }
+                        ModelState.AddModelError("", item.Description);
                     }
                 }
             }
-            return View(model);
         }
+
+        return View(model);
     }
 }
